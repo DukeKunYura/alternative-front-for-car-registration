50841fb4879051b5966d9cee731d3ec3658b8ad6
duke <dukekunyura@gmail.ru>
2023-04-14 22:51:26 +0300

    add masterSlice

---
+++ src/slices/masterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    search: "",
+    activeLink: "home",
+    isActiveCarAdder: false
+};
+
+export const masterSlice = createSlice({
+    name: 'master',
+    initialState,
+    reducers: {
+
+        setSearch: (state, action) => {
+            state.search = action.payload
+        },
+        setActiveLink: (state, action) => {
+            state.activeLink = action.payload
+        },
+        setIsActiveCarAdder: (state, action) => {
+            state.isActiveCarAdder = action.payload
+        }
+
+    }
+})
+
+
+export const { setSearch, setActiveLink, setIsActiveCarAdder } = masterSlice.actions
+
+export default masterSlice.reducer
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { api } from '../api/api';
+import { masterReducer } from '../slices/masterSlice'
 
 export const store = configureStore({
     reducer: {
-        [api.reducerPath]: api.reducer
+        [api.reducerPath]: api.reducer,
+        master: masterReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
 })
